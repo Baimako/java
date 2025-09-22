f989587f35b0b878788e9a9e20b3c11273ff07d1
Baimako <sascha.bergenas@hotmail.com>
2025-09-22 17:10:21 +0200

    index.html och index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// console.log("Hello World");
+
+// let age = 30; /*let låter dig ändra eftersom, const går inte ändra, använd const om värdet inte ska ändras*/
+// age = 31;
+
+// console.log(age);
+
+const name = "Sascha";
+const age = "35";
+// const isCool = true;
+// const rating = 4,5;
+// const x = null;
+// const y = undefined;
+// let z;
+
+console.log(`My name is ${name} and I am ${age}`);
+
+const hello = `My name is ${name} and I am ${age}`;
+
+console.log(hello);
